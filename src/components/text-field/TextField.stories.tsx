0e7f1c492213c007d920dd7bbfa91bf5741eb5cf
Ivan Dolgikh <ivan.dolgikh1199@gmail.com>
2024-10-13 16:33:49 +0300

    feat: TextField component

---
+++ src/components/text-field/TextField.stories.tsx
@@ -0,0 +1,104 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { TextField } from './TextField'
+
+const meta = {
+  argTypes: {
+    disabled: {
+      control: {
+        type: 'boolean',
+      },
+      table: {
+        defaultValue: { summary: 'false' },
+        type: {
+          summary: 'boolean',
+        },
+      },
+    },
+    error: {
+      control: {
+        type: 'text',
+      },
+      table: {
+        type: {
+          summary: 'string',
+        },
+      },
+    },
+    isRequired: {
+      control: {
+        type: 'boolean',
+      },
+      table: {
+        defaultValue: { summary: 'false' },
+        type: {
+          summary: 'boolean',
+        },
+      },
+    },
+
+    placeholder: {
+      control: { type: 'text' },
+      table: {
+        type: {
+          summary: 'string',
+        },
+      },
+    },
+    variant: {
+      control: { type: 'select' },
+      options: ['password', 'search', 'text'],
+      table: {
+        defaultValue: { summary: 'text' },
+        type: {
+          summary: 'string',
+          detail: `password', 'search', 'text`,
+        },
+      },
+    },
+    label: {
+      table: {
+        type: {
+          summary: 'string',
+        },
+      },
+    },
+  },
+  component: TextField,
+  tags: ['autodocs'],
+  title: 'components/TextField',
+} satisfies Meta<typeof TextField>
+
+export default meta
+type Story = StoryObj<typeof TextField>
+
+export const Text: Story = {
+  args: {
+    label: 'Some text-field label',
+    placeholder: 'type something...',
+  },
+}
+
+export const Password: Story = {
+  args: {
+    label: 'Some text-field label',
+    placeholder: 'type password...',
+    variant: 'password',
+  },
+}
+
+export const Search: Story = {
+  args: {
+    label: 'Some text-field label',
+    placeholder: 'type to start search...',
+    variant: 'search',
+  },
+}
+
+export const TextWithError: Story = {
+  args: {
+    error: 'Some error occurred...',
+    isRequired: true,
+    label: 'Some text-field label',
+    placeholder: 'type something...',
+  },
+}

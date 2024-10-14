5e669e15f9c81028d52893922c687803c2d227d2
Ivan Dolgikh <ivan.dolgikh1199@gmail.com>
2024-10-14 14:21:53 +0300

    feat: add badge component & version update to 0.4.0

---
+++ src/components/badge/Badge.stories.tsx
@@ -0,0 +1,44 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { BellOutlineIcon } from '../../icons'
+import { Badge } from './Badge'
+
+const meta = {
+  argTypes: {
+    children: {
+      control: { disable: true },
+      table: {
+        type: {
+          summary: 'any',
+        },
+      },
+    },
+    count: {
+      control: { type: 'number' },
+    },
+    variant: {
+      control: { type: 'inline-radio' },
+      options: ['dot', 'standard'],
+      table: {
+        defaultValue: { summary: 'standard' },
+        type: {
+          detail: `'dot', 'standard'`,
+          summary: 'string',
+        },
+      },
+    },
+  },
+  component: Badge,
+  tags: ['autodocs'],
+  title: 'Components/Badge',
+} satisfies Meta<typeof Badge>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const BadgeOnBellIcon: Story = {
+  args: {
+    children: <BellOutlineIcon height={'24px'} width={'24px'} />,
+    count: 10,
+  },
+}

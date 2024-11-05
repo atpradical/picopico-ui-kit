5da52e12b17581dc99089b2b9e2910ad183444be
Ivan Dolgikh <ivan.dolgikh1199@gmail.com>
2024-11-05 22:12:23 +0300

    feat: Add aspect ratio icons
    Added new icon components for AspectVerticalRectangle, AspectHorizontalRectangle, and AspectSquare along with their exports. Updated the main icons index to include the new aspect-ratio icons.

---
+++ src/icons/aspect-ratio/index.ts
@@ -0,0 +1,3 @@
+export { default as AspectHorizontalRectangle } from './AspectHorizontalRectangleIcon'
+export { default as AspectRatioIcon } from './AspectSquareIcon'
+export { default as AspectVerticalRectangle } from './AspectVerticalRectangleIcon'
